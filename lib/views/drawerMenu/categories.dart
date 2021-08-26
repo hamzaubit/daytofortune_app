5f5139b21b5965b135656152727da9220bd0af8a
hamzaubit <syedjgomi@gmail.com>
2021-08-26 16:58:32 +0500

    initial commit

--- lib/views/drawerMenu/categories.dart
+++ lib/views/drawerMenu/categories.dart
@@ -1,16 +1,13 @@
 import 'package:daytofortune_app/widgets/colorClass.dart';
 import 'package:daytofortune_app/widgets/sizeconfig.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'drawerMenu/categories.dart';
-
-class homeScreen extends StatefulWidget {
+class categories extends StatefulWidget {
   @override
-  _homeScreenState createState() => _homeScreenState();
+  _categoriesState createState() => _categoriesState();
 }
 
-class _homeScreenState extends State<homeScreen> {
+class _categoriesState extends State<categories> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -21,7 +18,7 @@ class _homeScreenState extends State<homeScreen> {
       drawer: Drawer(
         child: Container(
           decoration: BoxDecoration(
-              border: Border.all(color: textColor,width: 0.5),
+            border: Border.all(color: textColor,width: 0.5),
             color: drawerColor,
           ),
           child: ListView(
@@ -30,7 +27,7 @@ class _homeScreenState extends State<homeScreen> {
             children: [
               const DrawerHeader(
                 decoration: BoxDecoration(
-                  color: drawerColor
+                    color: drawerColor
                 ),
                 child: Center(
                   child: Text(
@@ -39,19 +36,17 @@ class _homeScreenState extends State<homeScreen> {
                   ),
                 ),
               ),
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => categories()));
-                },
-                child: Container(
-                  decoration: BoxDecoration(
+              Container(
+                decoration: BoxDecoration(
                     border: Border.all(color: textColor,width: 0.5)
-                  ),
-                  child: ListTile(
-                    tileColor: drawerColor,
-                    title: const Text('Categories',
-                      style: TextStyle(color: textColor,fontSize: 14),),
-                  ),
+                ),
+                child: ListTile(
+                  tileColor: drawerColor,
+                  title: const Text('Categories',
+                    style: TextStyle(color: textColor,fontSize: 14),),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ),
               Container(
@@ -188,63 +183,6 @@ class _homeScreenState extends State<homeScreen> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: 3,
-            itemBuilder: (context , index){
-              return quotesGetter();
-            },
-          ),
-        ),
-      ),
     );
   }
 }
-class quotesGetter extends StatefulWidget {
-  @override
-  _quotesGetterState createState() => _quotesGetterState();
-}
-
-class _quotesGetterState extends State<quotesGetter> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: Row(
-              children: [
-                SizedBox(width: SizeConfig.blockSizeHorizontal! * 80),
-                Icon(Icons.logout,color: Colors.indigo[600],size: SizeConfig.blockSizeHorizontal! * 7,),
-                SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
-                Icon(Icons.favorite_outlined,color: Colors.red,size: SizeConfig.blockSizeHorizontal! * 7,),
-                SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
-              ],
-            ),
-          ),
-          SizedBox(height: SizeConfig.blockSizeVertical! * 3,),
-          Padding(
-            padding: const EdgeInsets.only(left: 35),
-            child: Text("THE ONLY THING\nSTANDING BETWEEN YOU\nAND YOUR GOAL IS THE\nBULLSHIT STORY YOU KEEP\nTELLING YOURSELF\nAS TO WHY YOU CAN'T\nACHIEVE IT.",
-              style: GoogleFonts.poppins(color: Colors.white,fontSize: SizeConfig.blockSizeHorizontal! * 5.5),
-            ),
-          ),
-          SizedBox(height: SizeConfig.blockSizeVertical! * 2,),
-          Text("─JORDAN BELFORT",style: GoogleFonts.poppins(color: secondaryThemeColor,fontSize: SizeConfig.blockSizeHorizontal! * 4),),
-          SizedBox(height: SizeConfig.blockSizeVertical! * 12,),
-        ],
-      ),
-    );
-  }
-}
-
-
-/*Icon(Icons.logout,color: Colors.indigo[600],size: SizeConfig.blockSizeHorizontal! * 7,),
-SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
-Icon(Icons.favorite_outlined,color: Colors.red,size: SizeConfig.blockSizeHorizontal! * 7,),
-SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),*/
